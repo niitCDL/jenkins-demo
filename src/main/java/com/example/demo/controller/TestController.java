ce78795db3eb764bfe220e345609415b08db45b4
niitCDL <1952932103@qq.com>
2024-01-18 12:02:36 +0800

    init

---
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,23 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author : lenovo
+ * @program : demo
+ * @description :
+ * @create : 2024-01-17 14:17
+ **/
+@RestController
+public class TestController {
+    @GetMapping("/test")
+    public String test(){
+        return "hello jenkins!!!";
+    }
+
+    @GetMapping("/test2")
+    public String test2(){
+        return "hello jenkins!!!";
+    }
+}
